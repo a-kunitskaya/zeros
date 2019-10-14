427027813e7105dde8773b5ad864a54e01f99cfa
Alexandra <Alexandra.kunitskaya@gmail.com>
2019-10-14 10:16:50 +0300

    init: start implementing factorials

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function zeros(expression) {
-  // your solution
+    const single = expression.split('!').split('*');
+
+    const singleFactorial = single.reduce((acc, value) => {
+        if (value !== '') {
+            for (let i = value; i > 1; i--) {
+                acc *= i;
+            }
+        }
+        return acc;
+    }, 1);
+
+
+    const factorialStr= singleFactorial.toString();
+    return  factorialStr.substr(factorialStr.indexOf('0'), factorialStr.length).length;
 }
