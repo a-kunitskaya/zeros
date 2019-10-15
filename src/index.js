d7619a621d26e73956851b50369e39c98897aa27
Alexandra <Alexandra.kunitskaya@gmail.com>
2019-10-15 07:37:14 +0300

    feat: implement counting zeros with 2 & 5 division algorithm

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 module.exports = function zeros(expression) {
-    const single = expression.split('!').split('*');
+    const multipliers = expression.split("*");
 
-    const singleFactorial = single.reduce((acc, value) => {
-        if (value !== '') {
-            for (let i = value; i > 1; i--) {
-                acc *= i;
+    let twoDivisibleCount = 0;
+    let fiveDivisibleCount = 0;
+    multipliers.forEach(mul => {
+        let step = mul.includes("!!") ? 2 : 1;
+        let value = mul.match(/([\d]+)/g)[0];
+
+        while (value >= 2) {
+            let div = 5;
+            //count values divisible by 5
+            while (div <= value) {
+                if (value % div === 0) fiveDivisibleCount += 1;
+                div *= 5;
             }
+            //count values divisible by 2
+            if (value % 2 === 0) twoDivisibleCount += 1;
+            value -= step;
         }
-        return acc;
-    }, 1);
-
-
-    const factorialStr= singleFactorial.toString();
-    return  factorialStr.substr(factorialStr.indexOf('0'), factorialStr.length).length;
-}
+    });
+    return twoDivisibleCount > fiveDivisibleCount ? fiveDivisibleCount : twoDivisibleCount;
+};
